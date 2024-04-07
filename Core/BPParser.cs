ee36a1eed7f19a888f764551c8e0925d1c3981ac
Valacor <valyrious.informatics@gmail.com>
2024-04-07 21:29:39 +0200

    Extract struct to separate file

--- Core/BPParser.cs
+++ Core/BPParser.cs
@@ -18,21 +18,6 @@ public enum ParserErrorType
     ParserIsDead
 }
 
-public readonly struct ParserError
-{
-    public ParserErrorType Type { get; }
-
-    public string Message { get; }
-
-    public ParserError(
-        ParserErrorType type, 
-        string message)
-    {
-        Type = type;
-        Message = message;
-    }
-}
-
 public delegate void OnParserSuccess(
     BPContext context);
 
---
+++ Core/ParserError.cs
@@ -0,0 +1,16 @@
+﻿namespace Booky.Core;
+
+public readonly struct ParserError
+{
+    public ParserErrorType Type { get; }
+
+    public string Message { get; }
+
+    public ParserError(
+        ParserErrorType type,
+        string message)
+    {
+        Type = type;
+        Message = message;
+    }
+}
